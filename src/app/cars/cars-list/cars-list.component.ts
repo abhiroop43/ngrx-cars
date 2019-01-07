d5c1e4832e501893d3c2a6b290dff638444bc084
Abhiroop Santra <abhiroop.santra@gmail.com>
2019-01-07 08:53:22 +0400

    Implemented delete car

--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { IAppState } from 'src/app/store/state/app.state';
 import { Router } from '@angular/router';
-import { GetCars } from 'src/app/store/actions/car.actions';
+import { GetCars, DeleteCar } from 'src/app/store/actions/car.actions';
 import { selectCarList } from 'src/app/store/selectors/car.selectors';
+import { ICar } from 'src/app/models/car.model';
 
 @Component({
   selector: 'app-cars-list',
@@ -20,5 +21,13 @@ export class CarsListComponent implements OnInit {
 
   navigateToCar(id: number) {
     this._router.navigate(['car', id]);
+  }
+
+  deleteCar(car: ICar) {
+    const confirm_delete = confirm('Are you sure you want to delete this car?');
+    if (confirm_delete) {
+      console.log('Deleting.....', car);
+      this._store.dispatch(new DeleteCar(car));
+    }
   }
 }
